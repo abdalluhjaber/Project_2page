a452513abcf08b145db184bb87ca8e2144b112c8
abdalluhjaber <143411177+abdalluhjaber@users.noreply.github.com>
2023-09-11 20:07:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var div = document.getElementById('main');
+var dispiay = 0;
+
+
+function hideshow()
+{
+    if(dispiay == 1)
+    {
+        div.style.display = 'block';
+        display = 0;
+    }
+    else
+    {
+        div.style.display = 'none';
+        display = 1;
+
+    }
+
+}
